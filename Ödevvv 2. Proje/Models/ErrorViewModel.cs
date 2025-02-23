f8c48fcc26825d8aee70bfae5ecda10817309d7a
anil- <anil-@ANL>
2025-02-23 15:09:23 +0300

    Add project files.

---
+++ Ödevvv 2. Proje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ödevvv_2._Proje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
